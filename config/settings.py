845564957587f2ea32a62996a5c255b7d256aa88
AbduazimDev <20azimjonmahmudov03@gmail.com>
2023-06-20 21:34:02 +0500

    Update

--- config/settings.py
+++ config/settings.py
@@ -34,7 +34,7 @@ INSTALLED_APPS = [
 
 CKEDITOR_UPLOAD_PATH = 'uploads/'
 CKEDITOR_IMAGE_BACKEND = "pillow"
-CKEDITOR_JQUERY_URL = '//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js'
+CKEDITOR_JQUERY_URL = '//aja    x.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js'
 
 CKEDITOR_CONFIGS = {
     'default':
